773be8820c08c207624faae23930902c0f894ebb
jxyyyn <juda979854@gmail.com>
2023-10-15 23:46:04 +0900

    복습합시다

--- 알고리즘/최대 곱 구하기.py
+++ 알고리즘/최대 곱 구하기.py
@@ -1,19 +1,13 @@
-def max_product(card_lists):
-    product = 1
-    for card_list in card_lists:
-        product *= max(card_list)
-    return product
+def min_fee(pages_to_print):
+    fee = 0
+    sorted_list = sorted(pages_to_print)
+    for i in range(len(sorted_list)):
+        fee += sorted_list[i] * (len(sorted_list)- i)
+    return fee
 
 
 # 테스트 코드
-test_cards1 = [[1, 6, 5], [4, 2, 3]]
-print(max_product(test_cards1))
-
-test_cards2 = [[9, 7, 8], [9, 2, 3], [9, 8, 1], [2, 8, 3], [1, 3, 6], [7, 7, 4]]
-print(max_product(test_cards2))
-
-test_cards3 = [[1, 2, 3], [4, 6, 1], [8, 2, 4], [3, 2, 5], [5, 2, 3], [3, 2, 1]]
-print(max_product(test_cards3))
-
-test_cards4 = [[5, 5, 5], [4, 3, 5], [1, 1, 1], [9, 8, 3], [2, 8, 4], [5, 7, 4]]
-print(max_product(test_cards4))
+print(min_fee([6, 11, 4, 1]))
+print(min_fee([3, 2, 1]))
+print(min_fee([3, 1, 4, 3, 2]))
+print(min_fee([8, 4, 2, 3, 9, 23, 6, 8]))
